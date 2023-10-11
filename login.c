78e733527a1ede592c4bd60730380c433ac0c4a6
H-S-RATHI <goluu.000111@gmail.com>
2023-10-11 20:41:49 +0530

    Updated Virson

--- login.c
+++ login.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
-#include <conio.h>
-#include <windows.h>
 #include <string.h>
 #include <ctype.h>
 #include <stdlib.h>
+
+#ifdef _WIN32
+#include <conio.h>
+#include <windows.h>
+#define CLEAR_SCREEN "cls"
+#else
+#include <stdio_ext.h> // For __fpurge on some non-Windows systems
+#define CLEAR_SCREEN "clear"
+#endif
 
 #define ENTER 13
 #define TAB 9
@@ -48,6 +55,7 @@ void takepassword(char pwd[50]) {
     int i = 0;
     char ch;
 
+#ifdef _WIN32
     while (1) {
         ch = getch();
 
@@ -64,15 +72,35 @@ void takepassword(char pwd[50]) {
             printf("*");
         }
     }
+#else
+    while (1) {
+        ch = getchar();
+
+        if (ch == ENTER || ch == TAB || ch == '\n') {
+            pwd[i] = '\0';
+            break;
+        } else if (ch == BCKSPC) {
+            if (i > 0) {
+                i--;
+                printf("\b \b");
+            }
+        } else {
+            pwd[i++] = ch;
+            printf("*");
+        }
+    }
+#endif
 }
 
 int main() {
     struct user user;
-    system("color 0b");
-    FILE *fp;
     char start = 'y';
     int opt;
     char password2[50];
+
+#ifdef _WIN32
+    system("color 0b");
+#endif
 
     printf("\n\t\t\t--------------WELCOME TO AUTHENTICATION SYSTEM-----------------");
 
@@ -84,7 +112,12 @@ int main() {
 
         printf("\n\n\t\t\t\t\tYour choice->");
         scanf("%d", &opt);
-        fgetc(stdin);
+
+#ifdef _WIN32
+        fflush(stdin); // Clear input buffer on Windows
+#else
+        __fpurge(stdin); // Clear input buffer on non-Windows
+#endif
 
         switch (opt) {
             case 1:
@@ -98,17 +131,16 @@ int main() {
                 takepassword(user.password);
                 printf("\n\t\t\t\t\tConfirm Your Password  :->");
                 takepassword(password2);
-                system("cls");
+                system(CLEAR_SCREEN);
                 printf("\n\t\t\t\t\tAccount Created Successfully");
                 if (!strcmp(user.password, password2)) {
                     generateUsername(user.email, user.username);
                     printf("\n\t\t\t\t\t----------------------------");
                     printf("\n\n\t\t\t\t\tYOUR USERNAME IS: %s\n", user.username);
-                    
 
                     // Open a file for writing
-                    fp = fopen("user_data.txt", "a");  //
-                                        if (fp != NULL) {
+                    FILE *fp = fopen("user_data.txt", "a");
+                    if (fp != NULL) {
                         // Write user information to the file
                         fprintf(fp, "Username: %s\nFull Name: %s\nEmail: %s\nPhone: %s\nPassword: %s\n\n",
                                 user.username, user.fullName, user.email, user.phone, user.password);
@@ -123,70 +155,64 @@ int main() {
                 }
                 break;
 
-                        
-            // Inside the case 2 block
-case 2:
-    system("cls");
-    printf("\n\t\t\t\t\tEnter Your Username  :->");
-    takeinput(user.username);
-    printf("\n\t\t\t\t\tEnter Your Password  :->");
-    takepassword(user.password);
+            case 2:
+                system(CLEAR_SCREEN); // Clear the screen
+                printf("\n\t\t\t\t\tEnter Your Username  :->");
+                takeinput(user.username);
+                printf("\n\t\t\t\t\tEnter Your Password  :->");
+                takepassword(user.password);
 
-    fp = fopen("user_data.txt", "r");  // Open the user data file for reading
-    int userFound = 0;
+                FILE *fp = fopen("user_data.txt", "r");  // Open the user data file for reading
+                int userFound = 0;
 
-    if (fp != NULL) {
-        char line[100];
-        while (fgets(line, sizeof(line), fp)) {
-            if (strstr(line, "Username:") != NULL) {
-                char savedUsername[50];
-                sscanf(line, "Username: %[^\n]", savedUsername);
+                if (fp != NULL) {
+                    char line[100];
+                    while (fgets(line, sizeof(line), fp)) {
+                        if (strstr(line, "Username:") != NULL) {
+                            char savedUsername[50];
+                            sscanf(line, "Username: %[^\n]", savedUsername);
 
-                if (strcmp(savedUsername, user.username) == 0) {
-                    fgets(line, sizeof(line), fp);  // Skip Full Name line
-                    system("cls");
-                    printf("\n\n\t\t\t\t\t--------User Details--------");
-                    printf("\n\t\t\t\t\t----------------------------");
-                    printf("\n\n\t\t\t\t\t%s", line);
+                            if (strcmp(savedUsername, user.username) == 0) {
+                                fgets(line, sizeof(line), fp);  // Skip Full Name line
+                                system(CLEAR_SCREEN);
+                                printf("\n\n\t\t\t\t\t--------User Details--------");
+                                printf("\n\t\t\t\t\t----------------------------");
+                                printf("\n\n\t\t\t\t\t%s", line);
 
-                    fgets(line, sizeof(line), fp);  // Skip Email line
-                    printf("\t\t\t\t\t%s", line);
+                                fgets(line, sizeof(line), fp);  // Skip Email line
+                                printf("\t\t\t\t\t%s", line);
 
-                    fgets(line, sizeof(line), fp);  // Skip Phone line
-                    printf("\t\t\t\t\t%s", line);
-                    
-                    fgets(line, sizeof(line), fp);  // Get Password line
-                    char savedPassword[50];
-                    sscanf(line, "Password: %[^\n]", savedPassword);
-                    
-                    if (strcmp(savedPassword, user.password) == 0) {
-                        userFound = 1;
-                        break;
+                                fgets(line, sizeof(line), fp);  // Skip Phone line
+                                printf("\t\t\t\t\t%s", line);
+
+                                fgets(line, sizeof(line), fp);  // Get Password line
+                                char savedPassword[50];
+                                sscanf(line, "Password: %[^\n]", savedPassword);
+
+                                if (strcmp(savedPassword, user.password) == 0) {
+                                    userFound = 1;
+                                    break;
+                                }
+                            }
+                        }
                     }
+                    fclose(fp);
+                    printf("\n\n\t\t\t\t\tPress Enter to continue...");
+                    getchar(); // Pause until Enter is pressed
+                    system(CLEAR_SCREEN); // Clear the screen
+
+                    if (userFound) {
+                        printf("\n\n\t\t\t\t\t--------Login Successful--------\n");
+                    } else {
+                        printf("\n\n\t\t\t\t\t\tLogin failed.\n");
+                    }
+                } else {
+                    printf("\n\n\t\t\t\t\t\tFailed to open file for reading.\n");
                 }
-            }
-        }
-        fclose(fp);
-        Sleep(2000);
-
-        if (userFound) {
-            printf("\n\n\t\t\t\t\t--------Login Successful--------\n");
-        } else {
-            printf("\n\n\t\t\t\t\t\tLogin failed.\n");
-        }
-    } else {
-        printf("\n\n\t\t\t\t\t\tFailed to open file for reading.\n");
-    }
-    break;
-
-
-
-
-
-
+                break;
 
             case 3:
-                system("cls");
+                system(CLEAR_SCREEN); // Clear the screen
                 printf("\n\n\t\t\t\t\t----------------------------\n");
                 printf("\t\t\t\t\t-----Exiting the program-----\n");
                 printf("\t\t\t\t\t----------------------------");
@@ -201,4 +227,3 @@ case 2:
 
     return 0;
 }
-
